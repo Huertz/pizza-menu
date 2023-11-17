12f0ae25887a479653b38c6e08ab76953d3ff70c
Huertz <104065687+Huertz@users.noreply.github.com>
2023-11-17 00:11:07 -0600

    updated main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 function App() {
-  return <h1>Hello React!!</h1>
+  return <h1>Hello React</h1>;
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
