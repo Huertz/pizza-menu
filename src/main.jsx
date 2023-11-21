4581bf53613e64232beb674600cd96cc8ad2eba4
Huertz <104065687+Huertz@users.noreply.github.com>
2023-11-21 10:52:29 -0600

    intro to props

--- src/main.jsx
+++ src/main.jsx
@@ -75,21 +75,31 @@ function Menu() {
         name='Pizza Spinaci'
         ingredients='Tomato, mozarella, spinach, and ricotta cheese'
         photoName='pizzas/spinaci.jpg'
-        price='10'
+        price={10}
+      />
+      <Pizza
+        name='Pizza Funghi'
+        ingredients='Tomaro, mushrooms'
+        price={12}
+        photoName='pizzas/funghi.jpg'
       />
     </main>
   );
 }
 
 function Pizza(props) {
+  console.log(props);
   return (
-    <div>
+    <div className='pizza'>
       <img
-        src='pizzas/spinaci.jpg'
-        alt='pizza spinaci'
+        src={props.photoName}
+        alt={props.name}
       />
-      <h3>Pizza Spinacii</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
     </div>
   );
 }
