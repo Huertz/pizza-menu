9afe496ae7c3ea99db005e6908318ea20bf25e3e
Huertz <104065687+Huertz@users.noreply.github.com>
2023-12-02 00:59:03 -0600

    ? : conditional

--- src/main.jsx
+++ src/main.jsx
@@ -70,17 +70,23 @@ function Header() {
 
 function Menu() {
   const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
   return (
     <main className='menu'>
       <h2>Our Menu</h2>
-      <ul className='pizzas'>
-        {pizzaData.map((pizza) => (
-          <Pizza
-            pizzaObj={pizza}
-            key={pizza.name}
-          />
-        ))}
-      </ul>
+
+      {numPizzas > 0 ? (
+        <ul className='pizzas'>
+          {pizzaData.map((pizza) => (
+            <Pizza
+              pizzaObj={pizza}
+              key={pizza.name}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later</p>
+      )}
     </main>
   );
 }
@@ -111,10 +117,17 @@ function Footer() {
 
   return (
     <footer className='footer'>
-      {isOpen && (
+      {isOpen === true ? (
         <div className='order'>
           <p>We're open until {closeHour}.00. Come visit us or order online</p>
           <button className='btn'>Order</button>
+        </div>
+      ) : (
+        <div className='order'>
+          <p>
+            We're currently closed happy tp welcome you between {openHour}.00
+            and {closeHour}.00
+          </p>
         </div>
       )}
     </footer>
