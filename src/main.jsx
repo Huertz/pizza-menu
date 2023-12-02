43bd437dc39b288612d2106d68bf232906943e4a
Huertz <104065687+Huertz@users.noreply.github.com>
2023-12-02 02:02:20 -0600

    soldout

--- src/main.jsx
+++ src/main.jsx
@@ -98,10 +98,11 @@ function Menu() {
 }
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
 
   return (
-    <li className='pizza'>
+    // class name for css
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
       <img
         src={pizzaObj.photoName}
         alt={pizzaObj.name}
@@ -109,7 +110,7 @@ function Pizza({ pizzaObj }) {
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span>
       </div>
     </li>
   );
